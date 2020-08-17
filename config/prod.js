2a41d6493020f19859619d61af43546c7b255f65
Paulhan14 <pauljasonhan@gmail.com>
2020-08-17 16:08:37 -0400

    Deleted keys in comments

--- config/prod.js
+++ config/prod.js
@@ -9,7 +9,3 @@ module.exports = {
   sendGridKey: process.env.SEND_GRID_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN,
 };
-// prod name: dbUser, key: GHuVPjMhNTGDezip
-// mongodb+srv://dbUser:GHuVPjMhNTGDezip@cluster0-4bdnr.mongodb.net/emaily-prod?retryWrites=true&w=majority
-// prod client id: 401082932140-qqvko2nko5t793nseolshs16p9l9sijd.apps.googleusercontent.com
-// prod client secret: Bmu9M7q1d3TeH4YFYO3A82O7
